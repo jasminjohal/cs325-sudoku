aa243aadcafe05bc623ab0ef246cdf5a4d22c958
jjrx <jjohal52@gmail.com>
2022-06-24 18:37:02 -0700

    removed commented-out code

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ var sampleTable = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 9, 0, 0, 0, 0, 0, 7, 8],
   [7, 0, 3, 4, 0, 0, 5, 6, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
 // var sampleTable = [
@@ -27,37 +27,37 @@ function createTable(n) {
   /* Accepts an integer n and creates an nxn grid on the DOM to
   represent Sudoku board */
 
-  var grid = document.getElementById('grid');
+  var grid = document.getElementById("grid");
   // iterate over rows
   for (var i = 0; i < n; i++) {
-    var row = document.createElement('div');
-    row.classList.add('row');
+    var row = document.createElement("div");
+    row.classList.add("row");
     // iterate over columns
     for (var j = 0; j < n; j++) {
-      var box = 1
-      var cell = document.createElement('div');
-      cell.classList.add('cell');
+      var box = 1;
+      var cell = document.createElement("div");
+      cell.classList.add("cell");
 
       // add thick border lines
       if (j == 2 || j == 5) {
-        cell.style.borderRight = '3px solid black';
+        cell.style.borderRight = "3px solid black";
       }
       if (i == 2 || i == 5) {
-        cell.style.borderBottom = '3px solid black';
+        cell.style.borderBottom = "3px solid black";
       }
 
       // assign each cell a 3x3 box class depending on its location
       // box classes range from box-1 (upper left) to box-9 (bottom right)
       if (i > 5) {
-        box = 7
+        box = 7;
       } else if (i > 2) {
-        box = 4
+        box = 4;
       }
 
       if (j > 5) {
-        box += 2
+        box += 2;
       } else if (j > 2) {
-        box += 1
+        box += 1;
       }
 
       cell.classList.add(`box-${box}`);
@@ -65,16 +65,16 @@ function createTable(n) {
       // add a number to cell if its corresponding cell in sampleTable is not 0
       // these cells are 'default' (they cannot be changed)
       if (sampleTable[i][j] != 0) {
-        cell.classList.add('default');
+        cell.classList.add("default");
         var content = document.createTextNode(`${sampleTable[i][j]}`);
         cell.appendChild(content);
         // cells with value '0' are editable (user can change their value)
       } else {
-        cell.classList.add('editable');
+        cell.classList.add("editable");
       }
 
       // give each cell a unique id based on its position
-      cell.setAttribute('id', `${i},${j}`);
+      cell.setAttribute("id", `${i},${j}`);
       row.appendChild(cell);
     }
 
@@ -82,19 +82,19 @@ function createTable(n) {
   }
 
   // add click event listener for each 'editable' cell so user can enter in value into cell
-  var cells = document.getElementsByClassName('cell');
+  var cells = document.getElementsByClassName("cell");
   for (var c = 0; c < cells.length; c++) {
     // only 'editable' cells can be changed; 'default' cells cannot be changed
-    if (!cells[c].classList.contains('default')) {
-      cells[c].addEventListener('click', function(e) {
+    if (!cells[c].classList.contains("default")) {
+      cells[c].addEventListener("click", function (e) {
         e.preventDefault();
         // extra position from id since each cell has id 'i,j' where i is row, j is col
-        var loc = this.id.split(',');
+        var loc = this.id.split(",");
         var row = loc[0];
         var col = loc[1];
         // get user's input and place it into cell if valid
         getInputForCell(row, col);
-      })
+      });
     }
   }
 }
@@ -103,17 +103,17 @@ function validAddition(val, row, col) {
   /* Return true if the user input into a cell is valid based on Sudoku constraints;
   return false otherwise. Display helpful messages if false. */
 
-  var msg = ''
+  var msg = "";
   // checks if the input already exists in the selected cell's row
   if (sampleTable[row].includes(val)) {
-    updateAlert('Number already exists in row.');
+    updateAlert("Number already exists in row.");
     return false;
   }
 
   // checks if the input already exists in the selected cell's column
   for (var r = 0; r < 9; r++) {
     if (sampleTable[r][col] == val) {
-      updateAlert('Number already exists in column.');
+      updateAlert("Number already exists in column.");
       return false;
     }
   }
@@ -124,7 +124,7 @@ function validAddition(val, row, col) {
   var cellsInBox = document.getElementsByClassName(boxClass);
   for (var c = 0; c < cellsInBox.length; c++) {
     if (cellsInBox[c].textContent == val) {
-      updateAlert('Number already exists in 3x3 box.');
+      updateAlert("Number already exists in 3x3 box.");
       return false;
     }
   }
@@ -153,14 +153,18 @@ function updateCell(row, col, input, addValue) {
   }
 
   // clear the alert box
-  updateAlert('');
+  updateAlert("");
 }
 
 function getInputForCell(row, col) {
   /* Get input from user to enter into cell at position row, col. Update grid
   if input is valid (i.e. is an integer between 0 and 9 and meets grid constraints) */
 
-  var input = parseInt(prompt('Enter a number between 1 and 9 or enter 0 to clear existing number in cell: '));
+  var input = parseInt(
+    prompt(
+      "Enter a number between 1 and 9 or enter 0 to clear existing number in cell: "
+    )
+  );
 
   // if user input is valid integer, check if it meets grid constraints
   if (Number.isInteger(input) && input >= 0 && input <= 9) {
@@ -172,13 +176,13 @@ function getInputForCell(row, col) {
     }
     // user input is not valid integer
   } else {
-    updateAlert('That is not a valid integer!');
+    updateAlert("That is not a valid integer!");
   }
 }
 
 function updateAlert(msg) {
   /* Pass in a string 'msg' and display this in the div with id 'alert' */
-  var alertDiv = document.getElementById('alert');
+  var alertDiv = document.getElementById("alert");
 
   // remove the existing message if applicable
   if (alertDiv.firstChild) {
@@ -190,10 +194,10 @@ function updateAlert(msg) {
   alertDiv.appendChild(newMsg);
 
   // change color depending on message context
-  if (msg == 'Solved!') {
-    alertDiv.style.color = 'green';
+  if (msg == "Solved!") {
+    alertDiv.style.color = "green";
   } else {
-    alertDiv.style.color = '#eb4d4b';
+    alertDiv.style.color = "#eb4d4b";
   }
 }
 
@@ -202,16 +206,16 @@ function checkForDuplicates(val, i, j, grid, attribute) {
     (box, row, or col). Return false if so; return true otherwise */
 
   // extract 3x3 box class name to see what box it is in
-  if (attribute == 'boxes') {
+  if (attribute == "boxes") {
     var cell = document.getElementById(`${i},${j}`);
     var boxClass = cell.className.split(" ")[1];
     var key = boxClass;
-  // cell is in row i so look in grid['rows'][i]
-  } else if (attribute == 'rows') {
+    // cell is in row i so look in grid['rows'][i]
+  } else if (attribute == "rows") {
     var curRow = String(i);
     var key = curRow;
-  // cell is in col j so look in grid['cols'][j]
-  } else if (attribute == 'cols') {
+    // cell is in col j so look in grid['cols'][j]
+  } else if (attribute == "cols") {
     var curCol = String(j);
     var key = curCol;
   }
@@ -220,13 +224,13 @@ function checkForDuplicates(val, i, j, grid, attribute) {
   if (key in grid[attribute]) {
     // if the passed val is already in the list, we have found a duplicate
     if (grid[attribute][key].includes(val)) {
-      updateAlert('Invalid solution.');
+      updateAlert("Invalid solution.");
       return false;
-    // if the passed val is not in the list, add it to the list
+      // if the passed val is not in the list, add it to the list
     } else {
       grid[attribute][key].push(val);
     }
-  // add the specific attribute to the dictionary if it doesn't already exist
+    // add the specific attribute to the dictionary if it doesn't already exist
   } else {
     grid[attribute][key] = [val];
   }
@@ -244,7 +248,7 @@ function checkIfSolved() {
   var grid = {
     boxes: {},
     rows: {},
-    cols: {}
+    cols: {},
   };
 
   // iterate over each value in the grid and check if it already exists in its row/column/3x3 grid
@@ -255,43 +259,36 @@ function checkIfSolved() {
 
       // if a 0 exists in the grid, the puzzle has not been solved
       if (val == 0) {
-        updateAlert('Invalid solution.');
+        updateAlert("Invalid solution.");
         return false;
       }
 
       // check if duplicate values are in 3x3 grids, rows, or cols
       // return false if any of the constraints have been violated
-      if (!checkForDuplicates(val, i, j, grid, 'boxes')) {
+      if (!checkForDuplicates(val, i, j, grid, "boxes")) {
         return false;
       }
 
-      if (!checkForDuplicates(val, i, j, grid, 'rows')) {
+      if (!checkForDuplicates(val, i, j, grid, "rows")) {
         return false;
-
       }
-      if (!checkForDuplicates(val, i, j, grid, 'cols')) {
+      if (!checkForDuplicates(val, i, j, grid, "cols")) {
         return false;
       }
     }
   }
 
   // the grid met all constraints so the solution is valid
-  updateAlert('Solved!');
+  updateAlert("Solved!");
   confetti.start(2000);
   return true;
 }
 
-// validate user's soultion when he/she presses 'Check' button
-var checkButton = document.getElementById('check-button');
-checkButton.addEventListener('click', function(e) {
+// validate user's solution when he/she presses 'Check' button
+var checkButton = document.getElementById("check-button");
+checkButton.addEventListener("click", function (e) {
   e.preventDefault;
   checkIfSolved();
-})
-
-// var resetButton = document.getElementById('reset-button');
-// resetButton.addEventListener('click', function(e) {
-//   e.preventDefault;
-//   createTable(9);
-// })
+});
 
 createTable(9);
